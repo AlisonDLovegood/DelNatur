9811c4a7d721ea1c4810ecf6f894be76658dd44c
Alison Lovegood <alisondlovegood@gmail.com>
2022-02-28 20:46:54 -0300

    Sessão de contato, debug testemunhos e scroll

--- main.js
+++ main.js
@@ -46,3 +46,21 @@ const swiper = new Swiper('.swiper', {
   keyboard: true,
   loop: true
 })
+
+// SCROLL REVIEW
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+scrollReveal.reveal(
+  `
+  #home .image, #home .text,
+  #about .image, #about .text,
+  #services .header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
